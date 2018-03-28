ddd1182c31628ee7c712c25a9464c3e0b1e6f098
sromeiro <steven.romeiro@gmail.com>
2018-03-28 12:34:15 -0400

    building structure

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
 #include<stdlib.h>
 #include<stdexcept>
+#include<string>
 #include<fstream>
+
+#define FILENAME "input.txt" //Change file name here if needed.
 
 using namespace std;
 
 int main()
 {
+
+  ifstream inFile; //Create the input file object
+  inFile.open(FILENAME);
+
+  inFile.close(); //Closes the open input file
+
+
+
+
 
 
   //Successful termination of program
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
-all: main
+all: proj
 
-main: main.o
-	g++ -Wall main.o -o main
+proj: main.o
+	g++ -Wall main.o -o proj
 
 main.o: main.cpp
 	g++ -Wall -c main.cpp
 
 clean:
-	rm -rf *o main
+	rm -rf *o proj
