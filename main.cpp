bd4adb6b450fcda0f88b73d51cba410c9f839d47
sromeiro <steven.romeiro@gmail.com>
2018-04-01 15:47:39 -0400

    Final Code

--- main.cpp
+++ main.cpp
@@ -11,48 +11,9 @@ using namespace std;
 
 int main()
 {
-
-  //=============================TEST CODE====================================//
-  /*            UNCOMMENT THIS CODE TO TEST THE WORKIG ALGORITHM
-  int n, s, l;
-  cout << "Please enter the sampler size: ";
-  cin >> n;
-  cout << endl << "Please enter the variety of small candies: ";
-  cin >> s;
-  cout << endl << "Please enter the variety of long candies: ";
-  cin >> l;
-  cout << endl << "You have entered [" << s << "] small candies, [" << l << "]"
-       << " long candies for a sampler of size [" << n << "]" << endl;
-
-  int newValue, current, previous;
-
-  for(int i = 1; i <= n; i++)
-  {
-    if(i == 1)
-    {
-      previous = pow(s,i); //Raises base "s" to power of exponent "i"
-    }
-    else if(i == 2)
-    {
-      current = pow(s,i) + l; //Calculates s^i + l
-    }
-    else
-    {
-      newValue = current * s + previous * l;
-      previous = current;
-      current = newValue;
-    }
-  }
-
-  cout << "\nWe have created [" << current << "] samplers, each of size [" << n
-       << "], with the provided [" << s << "] small candies and [" << l
-       << "] long candies" << endl << endl;
-  */
-  //==========================================================================//
-
   int iterations; //To retrieve the amount of problems we'll run through
   int n, s, l; //To retrieve the sampler size, small and long parameters
-  int updated, current, previous; //To keep track of what's needed for this algo
+  long updated, current, previous; //To keep track of what's needed for this algo
 
   ifstream inFile; //Create the input file object
   inFile.open(FILENAME);
@@ -74,16 +35,30 @@ int main()
   for(int j = 1; j <= iterations; j++)
   {
     inFile >> n >> s >> l; //Grab sampler size, small and long candy values
-    previous = pow(s,1); //Initializes previous to s^1
-    current = pow(s,2) + l; //Initializes current to s^2 + l
 
-    for(int i = 3; i <= n; i++) //Main algorithm that calculates the final value
+    if(n == 1)
     {
-      updated = current * s + previous * l;
-      previous = current;
-      current = updated;
+      current = pow(s,1); //Initializes previous to s^1
     }
 
+    else if(n == 2)
+    {
+      current = pow(s,2) + l; //Initializes current to s^2 + l
+    }
+
+    else
+    {
+      previous = pow(s,1); //Initializes previous to s^1
+      current = pow(s,2) + l; //Initializes current to s^2 + l
+
+      for(int i = 3; i <= n; i++) //Main algorithm that calculates the final value
+      {
+        updated = current * s + previous * l;
+        previous = current;
+        current = updated;
+      }
+    }
+    
     outFile << current << endl; //Writes the final current value to the txt file
   }
 
